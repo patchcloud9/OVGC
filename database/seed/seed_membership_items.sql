82a870d983c30cfe8078afd1faed59d864d65a11
JJ Boettger <jj.boettger@gmail.com>
2026-02-24 12:05:01 -0800

    Add note to daily carts seed

--- database/seed/seed_membership_items.sql
+++ database/seed/seed_membership_items.sql
@@ -21,4 +21,4 @@ INSERT INTO membership_items (group_id,sort_order,name,price) VALUES
 ((SELECT id FROM membership_groups WHERE slug='other-prices'),1,'Yearly Cart Storage – Electric',300.00),
 ((SELECT id FROM membership_groups WHERE slug='other-prices'),2,'Yearly Cart Storage – Gas',250.00),
 ((SELECT id FROM membership_groups WHERE slug='other-prices'),3,'Yearly Trail Fee (Carts from home)',60.00),
-((SELECT id FROM membership_groups WHERE slug='other-prices'),4,'Daily Trail Fee (Carts from home)',12.00);
+((SELECT id FROM membership_groups WHERE slug='other-prices'),4,'Daily Trail Fee (Carts from home)',12.00,'may be waived for tournaments');
