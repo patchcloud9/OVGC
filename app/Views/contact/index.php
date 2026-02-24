ab54bb2f2abf2f010abfef0103b6c4fc622075d2
JJ Boettger <jj.boettger@gmail.com>
2026-02-23 16:32:51 -0800

    Layout contact page into two columns with info and form

--- app/Views/contact/index.php
+++ app/Views/contact/index.php
@@ -17,12 +17,12 @@ $layout = 'main';
             <div class="column is-half">
                 <h2 class="title is-4 has-text-centered">CONTACT INFO</h2>
                 <div class="content has-text-centered">
-                    <p><strong>Bear Creek Golf Course</strong></p>
-                    <p>19 Bear Creek Golf Course Road<br>Winthrop, WA 98862</p>
+                    <p><strong>Okanogan Valley Golf Club</strong></p>
+                    <p>115A Dankar Cutoff Road Omak<br>Omak, WA 9441</p>
                 </div>
                 <div class="notification is-light has-text-centered">
                     <span class="icon"><i class="fas fa-phone"></i></span>
-                    <a href="mailto:info@example.com">Email us</a> or call <a href="tel:5099962284">(509) 996-2284</a>
+                    <a href="mailto:info@example.com">Email us</a> or call <a href="tel:50998266937">(509) 826-6937/a>
                 </div>
             </div>
 
