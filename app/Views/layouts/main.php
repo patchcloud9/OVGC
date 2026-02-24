43b8a055e6ac7a1e24af115ccc6a22c488b1818e
JJ Boettger <jj.boettger@gmail.com>
2026-02-24 14:32:03 -0800

    Add error_log statement to output generated banner HTML for debugging

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -254,6 +254,8 @@ function rgba_from_hex(string $hex, float $alpha = 1.0): string
             $topHtml .= "<div class=\"banner notification is-$colour\" data-id=\"" . e($b['id']) . "\">$dismissHtml" . e($b['text']) . "</div>";
         }
         $topHtml .= "</div>";
+        // log generated HTML for debugging
+        error_log('BANNER HTML: ' . $topHtml);
     }
     ?>
 
